d12398aaaeca5b02ddddbe4147ed2fe7094effaf
Michael Sloan <mgsloan@gmail.com>
2019-11-17 22:45:16 -0700

    Fix scripts/process-jpg.sh

--- scripts/process-jpg.sh
+++ scripts/process-jpg.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -xe
 
-SCRIPT_DIR=${dirname $0}
+SCRIPT_DIR=`dirname $0`
 
 for ORIGINAL_PATH in "$@"
 do
   FILE=${ORIGINAL_PATH#original/}
 
-  $SCRIPT_DIR/resize-image.sh original/$FILE compressed/$FILE
+  $SCRIPT_DIR/resize-image.sh $ORIGINAL_PATH `dirname $ORIGINAL_PATH`/../images/$FILE
 
   # ./resize.sh original/$FILE resized/$FILE
   # /opt/mozjpeg/bin/cjpeg -outfile compressed/$FILE -quality 75 resized/$FILE
