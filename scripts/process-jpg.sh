635df44f71fa09140f400850739dc180be508066
Michael Sloan <mgsloan@gmail.com>
2019-11-18 21:28:36 -0700

    Improve process-jpg script to handle conversion from png and spaces in paths

--- scripts/process-jpg.sh
+++ scripts/process-jpg.sh
@@ -4,9 +4,13 @@ SCRIPT_DIR=`dirname $0`
 
 for ORIGINAL_PATH in "$@"
 do
-  FILE=${ORIGINAL_PATH#original/}
+  FILE="${ORIGINAL_PATH#original/}"
+  FILE_NO_EXT="${FILE%.*}"
 
-  $SCRIPT_DIR/resize-image.sh $ORIGINAL_PATH `dirname $ORIGINAL_PATH`/../images/$FILE
+  DEST_PATH="`dirname $ORIGINAL_PATH`/../images"
+
+  mkdir -p "$DEST_PATH"
+  "$SCRIPT_DIR/resize-image.sh" "$ORIGINAL_PATH" "$DEST_PATH/$FILE_NO_EXT.jpg"
 
   # ./resize.sh original/$FILE resized/$FILE
   # /opt/mozjpeg/bin/cjpeg -outfile compressed/$FILE -quality 75 resized/$FILE
