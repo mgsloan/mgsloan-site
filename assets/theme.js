ec8e9bdc22ddd22198c4da50b38acaa7ba220c3a
Michael Sloan <mgsloan@gmail.com>
2022-02-12 16:16:08 -0700

    Add use of utterances for commenting, with theme toggling iteration

---
+++ assets/theme.js
@@ -0,0 +1,39 @@
+(() => {
+  function getUtterancesTheme(mode) {
+    return mode === 'dark' ? 'github-dark' : 'github-light';
+  }
+
+  // Defer loading utterances until everything else is loaded.
+  // This way more important page content is prioritized.
+  window.addEventListener('load', () => {
+    let initialTheme = 'github-light';
+    const toggle = document.getElementsByTagName('dark-mode-toggle');
+    if (toggle.length !== 1) {
+      console.warn('Expected one dark-mode-toggle, but got: ', toggle);
+    } else {
+      initialTheme = getUtterancesTheme(toggle[0].mode);
+    }
+
+    // Add script that loads utterance
+    const commentsContainer = document.getElementById('comments');
+    if (commentsContainer) {
+      const s = document.createElement('script');
+      s.src = 'https://utteranc.es/client.js';
+      s.setAttribute('repo', 'mgsloan/mgsloan-site');
+      s.setAttribute('issue-term', 'pathname');
+      s.setAttribute('theme', initialTheme);
+      s.setAttribute('crossorigin', 'anonymous');
+      s.setAttribute('async', '');
+      commentsContainer.appendChild(s);
+    }
+  });
+
+  document.addEventListener('colorschemechange', (e) => {
+    const mode = e.detail.colorScheme;
+    const theme = getUtterancesTheme(mode);
+    document.documentElement.classList.toggle('dark', mode === 'dark');
+    for (const frame of document.getElementsByClassName('utterances-frame')) {
+      frame.contentWindow.postMessage({ type: 'set-theme', theme:  theme },  '*');
+    }
+  });
+})();
--- src/Main.hs
+++ src/Main.hs
@@ -302,6 +302,7 @@ regenerateCmd = do
   copyFile "assets/dark-mode-toggle/src/dark-mode-toggle.mjs" "out/dark-mode-toggle.mjs"
   copyFile "assets/dark-mode-toggle/src/dark-mode-toggle.mjs" "draft/out/dark-mode-toggle.mjs"
   copyFile "assets/redirect-index.html" "out/posts/index.html"
+  copyFile "assets/theme.js" "out/theme.js"
 
   putStrLn "Writing atom feed..."
   writeFeed (templates ! "feed.xml") posts baseConfig
